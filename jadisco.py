aa1981f8e82343323869aca71141db4656d70749
cbool22 <michal.czechura@gmail.com>
2015-11-15 14:27:39 +0100

    Update jadisco.py

--- jadisco.py
+++ jadisco.py
@@ -5,19 +5,38 @@ import random
 import urllib2
 import json
 import sys
+import os
+
 
 __module_name__ = 'Jadisco'
-__module_version__ = '1.0'
-__module_description__ = 'Skrypt do obslugi czatu Jadisco. Napisz /jd by uzyskac informacje o skrypcie.'
+__module_version__ = '2.1'
+__module_author__ = "cbool222"
+__module_description__ = 'Skrypt do obsługi czatu Jadisco. Napisz /jd by uzyskac informacje o skrypcie.'
 
-hexchat.prnt('Zaladowano Jadisco')
+hexchat.prnt('Załadowano Jadisco')
 
 colors = ['02','03','04','05','06','07','08','09','10','11','12','13']
+rainbow = [4,7,8,9,11,12,13,6]
+rainbow2 = ["04","07","08","09","11","12","13","06"]
 
-DEVELOPER_KEY = "YOUTUBE_API_KEY"
+DEVELOPER_KEY = "API_KEY"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
+slapfile = "C:/Users/cbool/AppData/Roaming/HexChat/addons/slapobjects.txt"
+
+def info(word, word_eol, user_data):
+    print("\002\00304Informacje skryptu Jadisco.")
+    print("")
+    print("\002KOMENDY")
+    print("1. \002\035dc\035\002 - Death Counter, wymaga argumentu czyli ilości śmierci.")
+    print("2. \002\035rng\035\002 - Status RNG Maćka, wpisz /rng info by uzyskać więcej informacji.")
+    print("3. \002\035mj\035\002 - MJUZIK!!!1one, wymaga podania linku do muzyki z YouTube.")
+    print("4. \002\035r\035\002 - Wypisuje tekst w kolorze tęczy")
+    print("5. \002\035tencza\035\002 - TENCZA!!!")
+    print("")
+    print("")
+    return hexchat.EAT_ALL 
 
 def death_counter(word, word_eol, user_data):
     if len(word_eol[1]) < 1:
@@ -26,8 +45,35 @@ def death_counter(word, word_eol, user_data):
         hexchat.command('say \002Death Counter: \00304' + word_eol[1])
     return hexchat.EAT_ALL
 
-def r_n_g(word, word_eol, user_data):
-    hexchat.command('say \002Status RNG: '+ "\003" +random.choice(colors) + word_eol[1])
+def rng(word, word_eol, user_data):
+    if len(word_eol) < 2:
+        f = open(slapfile,'r')
+        line = next(f)
+        for num, aline in enumerate(f):
+            if random.randrange(num + 2): continue
+            line = aline.rstrip()
+        hexchat.command("say \002Status RNG: " + "\003" +random.choice(colors) + line)        
+    elif word[1] == "add":
+        f = open(slapfile,"a")
+        f.write(word_eol[2]+"\n")
+        print("Dodano " + word_eol[2] + " do spisu")
+        f.close
+    elif word[1] == "info":
+        print("\002\00304Informacje o funkcji RNG.")
+        print("")
+        print("\002ARGUMENTY")
+        print("1. \002\035brak\035\002 - Wypisuje podany status z losowym kolorem")
+        print("2. \002\035add\035\002 - Dodaje podany status do spisu")
+        print("3. \002\035list\035\002 - Wypisuje wszystkie statusy z spisu")
+        print("")
+    elif word[1] == "list":
+        print("Lista statusów RNG")
+        print("")
+        lines = [line.rstrip('\n') for line in open(slapfile)]
+        for element in lines:
+            print(element)
+    else:
+        hexchat.command('say \002Status RNG: '+ "\003" +random.choice(colors) + word_eol[1])
     return hexchat.EAT_ALL
 
 def mj(word, word_eol, user_data):
@@ -39,17 +85,33 @@ def mj(word, word_eol, user_data):
         hexchat.command("say \002\00304Mjuzik: %s\003 :: \00309URL: https://youtu.be/"  % video['snippet']['title'] + id)
     return hexchat.EAT_ALL
 
-def info(word, word_eol, user_data):
-    print("\002\00304Informacje skryptu Jadisco.")
-    print("")
-    print("\002KOMENDY")
-    print("1. \002\035dc\035\002 - Death Counter, wymaga argumentu czyli ilosci smierci.")
-    print("2. \002\035rng\035\002 - Status RNG Macka, wymaga argumentu.")
-    print("3. \002\035mj\035\002 - MJUZIK!!!1one, wymaga podania linku do muzyki z YouTube.")
-    print("")
-    return hexchat.EAT_ALL   
+def Rainbow(text,offset):
+	rainbowed = ""
+
+	for i in range(len(text)):
+		rainbowed += "\x03{}{}".format(rainbow[(i+offset)%len(rainbow)], text[i])
+	return rainbowed
+
+def rainbowcmd(word,word_eol,userdata):
+	try:
+		count = int(word[-1])
+		text = ' '.join(word[1:-1])
+	except ValueError:
+		count = 1
+		text = ' '.join(word[1:])
+	for i in range(0,count):
+		(hexchat.get_context()).command("say {}".format(Rainbow(text,i)))
+
+def tencza(word, word_eol, user_data):
+    test = ""
+    for element in rainbow2:
+        test += "\003" + element +"█"
+    hexchat.command("say " + test)
+    return hexchat.EAT_ALL
 
 hexchat.hook_command('dc', death_counter)
-hexchat.hook_command('rng', r_n_g)
+hexchat.hook_command('rng', rng)
 hexchat.hook_command('mj', mj)
 hexchat.hook_command('jd', info)
+hexchat.hook_command('r', rainbowcmd)
+hexchat.hook_command('tencza', tencza)
